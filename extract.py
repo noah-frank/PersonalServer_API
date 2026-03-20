99880ec44f97fa4cb2bcf9e85c9fae129f8ecfbc
noah-frank <noahfrank0001@gmail.com>
2026-03-19 21:31:28 -0400

    got E and T working

--- extract.py
+++ extract.py
@@ -2,6 +2,8 @@ import yfinance
 import pandas as pd
 import os
 import glob
+from datetime import datetime
+import pytz
 
 base_path = os.getcwd()
 path = f"{base_path}/CSVs/raw_batch/*"
@@ -13,8 +15,12 @@ try:
 except Exception as err:
     print(err)
 
+start = datetime(2016, 3, 19, tzinfo=pytz.utc)
+end = datetime(2026, 3, 19, tzinfo=pytz.utc)
 
-df = yfinance.Ticker("AAPL").history(period="1y", interval="1d")
+ticker = "AAPL"
+df = yfinance.Ticker(ticker).history(start=start, end=end, interval="1d")
+df["Ticker"] = ticker
 
 print(df.head())
 
--- transform.py
+++ transform.py
@@ -1,16 +1,44 @@
-import pandas as pd
 import os
 import glob
-
+import pandas as pd
+import pandas_ta as ta
 base_path = f"{os.getcwd()}/CSVs"
-raw_batch = f"{base_path}/raw_batch"
-transformed_batch = f"{base_path}/transformed_batch/*"
+raw_batch = f"{base_path}/raw_batch/current_batch.csv"
+transformed_batch = f"{base_path}/transformed_batch"
 
 try:
-    files = glob.glob(path)
+    files = glob.glob(f"{transformed_batch}/*")
     for f in files:
         os.remove(f)
 except Exception as err:
     print(err)
 
-pd.read_csv("")
+df = pd.read_csv(raw_batch)
+
+### TRANSFORMATIONS
+
+df = df.drop(columns=["Dividends", "Stock Splits"])
+df.columns = ["date", "open", "high", "low", "close", "volume", "ticker"]
+df["date"] = pd.to_datetime(df["date"], utc=True)
+
+
+df["SMA10"] = ta.sma(df.close, length=10)
+df["SMA20"] = ta.sma(df.close, length=20)
+df["SMA30"] = ta.sma(df.close, length=30)
+df["AO"] = ta.ao(high=df.high, low=df.low)
+
+ichimoku_df = ta.ichimoku(
+    high=df['high'],
+    low=df['low'],
+    close=df['close'],
+    include_chikou=False,
+    lookahead=False
+)[0]
+
+df = pd.concat([df, ichimoku_df], axis=1)
+
+df = df.dropna()
+
+print(df.info())
+
+df.to_csv(f"{transformed_batch}/current_batch.csv")
